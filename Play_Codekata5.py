58e929b9adda4728131f89f3aba6d8d4db182759
SKSMKHari <hariprasathks6@gmail.com>
2018-05-21 13:54:19 +0530

    Update Play_Codekata5.py

--- Play_Codekata5.py
+++ Play_Codekata5.py
@@ -1,4 +1,4 @@
-def rom_to_int(string):
+def romtoint(string):
     table=[['M',1000],['CM',900],['D',500],['CD',400],['C',100],['XC',90],['L',50],['XL',40],['X',10],['IX',9],['V',5],['IV',4],['I',1]]
     returnint=0
     for pair in table:
