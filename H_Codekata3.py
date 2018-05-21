94842fac77e1b901e1c282b5f3bd773508a617a0
SKSMKHari <hariprasathks6@gmail.com>
2018-05-21 09:52:29 +0530

    Create H_Codekata3.py

---
+++ H_Codekata3.py
@@ -0,0 +1,19 @@
+flg=0
+v1=0
+opt=[]
+v2=[]
+a=int(input("Enter a Value:"))
+for i in range(0,a):
+    v2.append(int(input("Enter the Value:")))
+for i in v2:
+    if i==v1:
+        opt.append(i)
+        v1+=1
+        flg=1
+    else:
+        v1+=1
+if flg==0:
+    print (-1)
+else:
+    opt.sort(reverse=True)
+    print (opt)
